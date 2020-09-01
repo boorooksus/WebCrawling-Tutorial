8ba2aba48693377dfd6a0ebd90f05baa8ac1419d
boorooksus <boorooksus@gmail.com>
2020-09-01 10:06:58 +0900

    네이버 검색 결과 가져오기

---
+++ L03_NaverBlogResult.py
@@ -0,0 +1,18 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query=%ED%8C%8C%EC%9D%B4%EC%8D%AC'
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+title = soup.find_all(class_='sh_blog_title')
+print(title)
+print(len(title))
+title2 = soup.find(class_='sh_blog_title')
+print(title2)
+print(len(title2))
+
+for i in title:
+    print(i.attrs['title'])
+    print(i.attrs['href'])
+    print()
